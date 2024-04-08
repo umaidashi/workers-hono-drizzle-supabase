ccf8a8a0333124faddbcfc0f9f7ceffe54604107
umaidashi <umaidashi18@gmail.com>
2024-04-09 01:17:19 +0900

    fix: drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,19 @@
+import { config } from "dotenv";
 import type { Config } from "drizzle-kit";
 
+config({
+	path: ".dev.vars",
+});
+
 export default {
-  schema: "./src/db/schema.ts",
-  out: "./drizzle",
-  driver: "pg",
+	schema: "./src/db/schema.ts",
+	out: "./drizzle",
+	driver: "pg",
+	dbCredentials: {
+		user: process.env.DATABASE_USER as string,
+		host: process.env.DATABASE_HOST as string,
+		database: process.env.DATABASE_NAME as string,
+		password: process.env.DATABASE_PASSWORD as string,
+		connectionString: process.env.DATABASE_URL as string,
+	},
 } satisfies Config;
